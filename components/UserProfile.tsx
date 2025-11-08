6c31b046736604f8348d6c428a55fd471d261079
wildwingsbengal <wildwingsbengal@gmail.com>
2025-11-09 04:01:20 +0530

    menu change

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -127,7 +127,7 @@ const UserProfile: React.FC<UserProfileProps> = ({ user, onLogout, onEditProfile
               <svg className="w-4 h-4 mr-3 text-slate-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
               </svg>
-              Edit Profile
+              Profile ( Set Your API Key )
             </button>
 
             <button
